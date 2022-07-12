c61137803d2cd964ae54a571c3fc5fe49e36a2c2
Henrique Alves <henriqueadlima@hotmail.com>
2022-07-12 18:26:51 -0300

    Trabalhando com CMS

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,75 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
+import { getPrismicCleint } from '../../services/prismic'
+
+import styles from './styles.module.scss'
+
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>
+                {post.updatedAt}
+              </time>
+              <strong>
+                {post.title}
+              </strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicCleint()
+
+  const response = await prismic.query<any>([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['title', 'content'],
+    pageSize: 100
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.Title),
+      excerpt: post.data.Content.find(content => content.type === 'paragraph')?.text ?? '', //Procura dentro de content um conteudo com tipo paragrafo. Caso encontre, pega o texto dele, senao retorna vazio
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return {
+    props: { posts }
+  }
+}
